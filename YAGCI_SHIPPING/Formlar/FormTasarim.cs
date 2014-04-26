4a40a9b092ef1f48a6be1637bb2e817d7a834065
canavartoro <canavartoro@users.noreply.github.com>
2014-04-26 22:42:00 +0300

    sablon eklendi

--- YAGCI_SHIPPING/Formlar/FormTasarim.cs
+++ YAGCI_SHIPPING/Formlar/FormTasarim.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -81,7 +82,24 @@ namespace YAGCI_SHIPPING.Formlar
             try
             {
                 timerFormAc.Enabled = false;
+                FileStream fout = new FileStream(Application.StartupPath + "\\gemi.doc", FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite);
+                BinaryWriter bw = new BinaryWriter(fout);
+                bw.Write(Properties.Resources.bos);
+                bw.Flush();
+                bw.Close();
                 axOfficeViewer1.Open(Application.StartupPath + "\\gemi.doc");
+
+                Microsoft.Office.Interop.Word.DocumentClass doc = axOfficeViewer1.ActiveDocument as Microsoft.Office.Interop.Word.DocumentClass;
+                if (doc != null)
+                {
+                    doc.Content.InsertAfter("deneme");
+                }
+                
+                //Microsoft.Office.Interop.Word.DocumentClass
+
+                //axOfficeViewer1.CreateNew("Word.Document");
+                //OfficeViewer1. CreateNew "Excel.Sheet" 
+
             }
             catch (Exception exc)
             {
@@ -96,6 +114,7 @@ namespace YAGCI_SHIPPING.Formlar
                 axOfficeViewer1.ShowDialog(OfficeViewer.ShowDialogType.DialogSaveCopy);
             }
         }
+
         private void btnIceri_Click(object sender, EventArgs e)
         {
             axOfficeViewer1.ShowDialog(OfficeViewer.ShowDialogType.DialogOpen);
